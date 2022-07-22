ff3783c538b86ab0e98bf6f3199e4b73cb30b02d
Guillaume Smet <guillaume.smet@gmail.com>
2022-07-22 15:34:27 +0200

    Print context to check context in composite action

--- src/main/java/io/github/gsmet/githubactionplayground/PlaygroundAction.java
+++ src/main/java/io/github/gsmet/githubactionplayground/PlaygroundAction.java
@@ -2,15 +2,18 @@ package io.github.gsmet.githubactionplayground;
 
 import io.quarkiverse.githubaction.Action;
 import io.quarkiverse.githubaction.Commands;
+import io.quarkiverse.githubaction.Context;
 import io.quarkiverse.githubaction.Inputs;
 
 public class PlaygroundAction {
 
     @Action
-    void action(Inputs inputs, Commands commands) {
+    void action(Inputs inputs, Commands commands, Context context) {
         System.out.println(inputs.get("who-to-greet"));
 
         commands.setOutput("testKey", "testValue");
         commands.jobSummary("## Test :rocket:");
+
+        context.print();
     }
 }
