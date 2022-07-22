3976c407061480755ec6b0e7eba000024baee244
Guillaume Smet <guillaume.smet@gmail.com>
2022-07-22 10:20:30 +0200

    Cleanup

--- src/main/java/io/github/gsmet/githubactionplayground/PlaygroundAction.java
+++ src/main/java/io/github/gsmet/githubactionplayground/PlaygroundAction.java
@@ -1,7 +1,5 @@
 package io.github.gsmet.githubactionplayground;
 
-import java.util.Map.Entry;
-
 import io.quarkiverse.githubaction.Action;
 import io.quarkiverse.githubaction.Commands;
 import io.quarkiverse.githubaction.Inputs;
@@ -10,10 +8,6 @@ public class PlaygroundAction {
 
     @Action
     void action(Inputs inputs, Commands commands) {
-        for (Entry<String, String> envEntry : System.getenv().entrySet()) {
-           System.out.println("ENV: " + envEntry.getKey() + "=" + envEntry.getValue());
-        }
-
         System.out.println(inputs.get("who-to-greet"));
 
         commands.setOutput("testKey", "testValue");
